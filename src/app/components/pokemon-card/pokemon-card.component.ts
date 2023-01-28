e294d11cd0c85058e854d70654d38a49aefd5b50
caioorleans <caioorleansdm@hotmail.com>
2023-01-28 19:39:05 -0300

    pokemon card v2

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
-  styleUrls: ['./pokemon-card.component.css']
+  styleUrls: ['./pokemon-card.component.css','./type-style.component.css']
 })
 export class PokemonCardComponent {
-  types:string[] = ["fire", "dark"];
+  types:string[] = ["grass", "psychic"];
 }
